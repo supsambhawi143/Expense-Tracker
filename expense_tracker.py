b946f14dd5e20994ad3867f1ea6222e86bcd93c9
Sambhawi Paudel <sambhawipaudel.7@gmail.com>
2025-10-29 19:20:05 +0545

    Added function to record new expenses

--- expense_tracker.py
+++ expense_tracker.py
@@ -13,3 +13,26 @@ def initialize_file():
     except FileExistsError:
         pass
 
+def add_expense():
+    date = datetime.now().strftime("%Y-%m-%d")
+    category = category_entry.get()
+    amount = amount_entry.get()
+    description = description_entry.get()
+
+
+    if not category or not amount:
+        messagebox.showwarning("Input Error", "Please enter category and amount!")
+        return 
+    
+    try:
+        amount = float(amount)
+    except ValueError:
+        messagebox.showerror("Input Error", "Amount must be a number!")
+        return
+    
+    with open(Filename, 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([date, category, amount, description])
+
+    messagebox.showinfo("Success", "Expense recorded successfully!")
+    
